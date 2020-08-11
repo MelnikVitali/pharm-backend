4bd127a0d4362d6eff9ceb39f7422d8b31be0b37
MelnikVitali <mvitalim@gmail.com>
2020-08-12 00:06:38 +0300

    file upload limit 1m

--- utils/imageUploadAndResize.js
+++ utils/imageUploadAndResize.js
@@ -1,9 +1,14 @@
-// const sharp = require('sharp');
+const sharp = require('sharp');
+const path = require('path');
 // const { v4: uuidv4 } = require('uuid');
 
 // const uploadUrls = require('./../constans/uploadUrls');
 
 const resizeAndSaveImages = async (file) => {
+    const fileName = file.originalname;
+
+    const newFileName = fileName.substring(0, fileName.lastIndexOf('.')) || fileName;
+
     // const newFileId = uuidv4();
     //
     // await sharp(file.buffer)
@@ -18,23 +23,16 @@ const resizeAndSaveImages = async (file) => {
     //             console.log(`The file ${newFileId} was compressed with extension .${data.format} and saved`);
     //         });
     //
-    // await sharp(file.buffer)
-    //     // .resize(640, 320)
-    //     .png()
-    //     .toFile(`${uploadUrls.imageUploadUrl}/${newFileId}.png`,
-    //         (err, data) => {
-    //             if (err) {
-    //                 console.error(err);
-    //             }
-    //             console.log(`The file ${newFileId} was compressed with extension .${data.format} and saved`);
-    //         });
+    const resizedImage = await sharp(file.buffer)
+        .webp({ quality: 80 })
+        .toBuffer()
 
     return Promise.resolve({
-        imageName: file.originalname,
+        imageName: `${newFileName}.webp`,
         originalName: file.originalname,
         isImage: true,
         isRejected: false,
-        base64: Buffer.from(file.buffer, 'binary').toString('base64')
+        base64: Buffer.from(resizedImage, 'binary').toString('base64')
     });
 };
 
