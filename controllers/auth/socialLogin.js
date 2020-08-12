bd027ae006d73e64365a7fd58caebe7cae742f86
MelnikVitali <mvitalim@gmail.com>
2020-08-12 22:17:44 +0300

    facebook auth

--- controllers/auth/socialLogin.js
+++ controllers/auth/socialLogin.js
@@ -25,7 +25,7 @@ const googleLogin = (req, res) => {
 
 const facebookLogin = (req, res) => {
     const { userID, accessToken } = req.body;
-    const urlGraphFacebook = `https://graph.facebook.com/v2.11/${userID}/?fields=id,name,email&access_token=${accessToken}`;
+    const urlGraphFacebook = `https://graph.facebook.com/v2.11/${userID}/?fields=name,email,id&access_token=${accessToken}`;
 
     fetch(urlGraphFacebook, { method: 'GET' })
         .then(response => response.json())
